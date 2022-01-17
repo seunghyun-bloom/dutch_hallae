6e31752ae3e17ef0fea792ed0739b336604f3507
seunghyun kwon <seunghyun.bloom@gmail.com>
2022-01-18 00:55:40 +0900

    sh jan_17 1

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//TODO: [Hamzzy] build MainPage referring to figma (https://www.figma.com/file/MhCBjezpJL31DT2HXiyL9x/Untitled?node-id=54%3A2)
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
