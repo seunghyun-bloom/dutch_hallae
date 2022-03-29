6d6de2727aab89782d6818432a45f3520ddc2020
seunghyun kwon <seunghyun.bloom@gmail.com>
2022-03-30 01:48:47 +0900

    sh mar_29

--- lib/getx/controller/friends_controller.dart
+++ lib/getx/controller/friends_controller.dart
@@ -1,20 +1,24 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dutch_hallae/utilities/toast.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-//TODO: create method to upload data to cloud firestore
-
 final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
 final FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+String defaultImageURL =
+    'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_0.jpeg?alt=media&token=3f159e77-cade-4345-8b6e-7a5d4c7e75e8';
+String defaultImage = 'assets/images/friend_sample_0.jpeg';
 
 class FriendsController extends GetxController {
-  RxString friendImageTemp =
-      'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_0.jpeg?alt=media&token=3f159e77-cade-4345-8b6e-7a5d4c7e75e8'
-          .obs;
-  RxList<String> sampleFriendImages = [
+  RxString friendImageURL = defaultImageURL.obs;
+  RxString showingFriendImage = defaultImage.obs;
+  List<String> sampleFriendImages = [
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_0.jpeg?alt=media&token=3f159e77-cade-4345-8b6e-7a5d4c7e75e8',
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_1.jpeg?alt=media&token=3e418c93-3688-4256-ba80-f9b8d05488c5',
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_2.jpeg?alt=media&token=fdc0f4e5-56a6-4fe3-ad63-6f4993f479dd',
@@ -25,9 +29,22 @@ class FriendsController extends GetxController {
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_7.jpeg?alt=media&token=cdf99d99-1d53-4148-b99c-9579778bfc49',
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_8.jpeg?alt=media&token=970360fd-32fc-4677-b55e-3f33c732e1cf',
     'https://firebasestorage.googleapis.com/v0/b/dutchhallae.appspot.com/o/basic%2Ffriends%2Fsample_images%2Ffriend_sample_9.jpeg?alt=media&token=ffd0c33a-dc81-412a-b090-61dbd6630f6e'
-  ].obs;
+  ];
+  final CollectionReference _friendsRef = _firestore
+      .collection('userData')
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .collection('friends');
+  final Reference _refFriends = _firebaseStorage
+      .ref()
+      .child('user')
+      .child('${_auth.currentUser?.uid}')
+      .child('friends');
+
+  String downloadUrlTemp = '';
+  RxBool isSample = true.obs;
 
   getFriendImage(ImageSource source) async {
+    isSample(false);
     final ImagePicker _picker = ImagePicker();
     XFile? ximage = await _picker.pickImage(
       source: source,
@@ -35,34 +52,43 @@ class FriendsController extends GetxController {
       maxWidth: 300,
     );
 
-    File image = File(ximage!.path);
+    showingFriendImage(ximage!.path);
 
-    Reference reference = _firebaseStorage
-        .ref()
-        .child('user')
-        .child('${_auth.currentUser?.uid}')
-        .child('friends')
-        .child('temp')
-        .child(DateTime.now().toString());
-
-    UploadTask uploadTask = reference.putFile(image);
-
-    await uploadTask.whenComplete(() => null);
-
-    String downloadURL = await reference.getDownloadURL();
-
-    friendImageTemp(downloadURL);
     Get.back();
   }
 
   selectSampleImage(int i) {
+    isSample(true);
     String selectedSampleImage = sampleFriendImages[i];
-    friendImageTemp(selectedSampleImage);
+    friendImageURL(selectedSampleImage);
+    showingFriendImage('assets/images/friend_sample_$i.jpeg');
   }
 
-  @override
-  void onClose() {
-    friendImageTemp('');
-    super.onClose();
+  uploadFriendFirestore(String name, String phone) async {
+    Reference refSelected = _refFriends.child('selected_profile').child(name);
+
+    if (isSample.value == false) {
+      File friendImage = File(showingFriendImage.value);
+
+      UploadTask uploadTask = refSelected.putFile(friendImage);
+
+      await uploadTask.whenComplete(() => null);
+
+      String downloadURL = await refSelected.getDownloadURL();
+      friendImageURL(downloadURL);
+    }
+
+    await _friendsRef.doc(name).set({
+      'name': name,
+      'phone': phone,
+      'image': friendImageURL.value,
+    });
+
+    Get.back();
+
+    showToast('$name님의 정보가 업데이트 되었습니다.');
+
+    showingFriendImage(defaultImage);
+    isSample(true);
   }
 }
